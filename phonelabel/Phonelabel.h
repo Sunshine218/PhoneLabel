4c113a4448a719dc9a76183075d0cb23e73f1e5a
罗雪梅 <215206318@qq.com>
2016-12-05 13:34:59 +0800

    Release 1.0.0.

---
+++ phonelabel/Phonelabel.h
@@ -0,0 +1,15 @@
+//
+//  Phonelabel.h
+//  TestSmall
+//
+//  Created by geely on 16/12/5.
+//  Copyright © 2016年 geely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Phonelabel : UILabel
+
+- (void)printString;
+
+@end
---
+++ phonelabel/Phonelabel.m
@@ -0,0 +1,18 @@
+//
+//  Phonelabel.m
+//  TestSmall
+//
+//  Created by geely on 16/12/5.
+//  Copyright © 2016年 geely. All rights reserved.
+//
+
+#import "Phonelabel.h"
+
+@implementation Phonelabel
+
+- (void)printString
+{
+    NSLog(@"test cocoapods....");
+}
+
+@end
